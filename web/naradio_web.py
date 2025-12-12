4da14ec1376a101c0b63e4f3c2655ece72030469
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2025-12-12 11:12:07 +0100

    proxyfix

--- web/naradio_web.py
+++ web/naradio_web.py
@@ -15,7 +15,10 @@ from na_radio.manager import Manager
 
 print("DEBUG: Running modified naradio_web.py from bjoernl workspace")
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 app = Flask(__name__, static_folder='static', template_folder='templates')
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 # Global Manager instance
 manager = None
